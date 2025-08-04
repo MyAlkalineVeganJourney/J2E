1f07888917b9513b9e90cd35d458bcadf32392dd
Robin <Myalkalineveganjourney@gmail.com>
2025-08-04 17:21:46 -0400

    Updates

--- frontend/src/components/ChakraDiagram.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ChakraDiagram = () => (
-  <div className="chakra-diagram">
-    <img src="/images/ChakraFunctions.jpg" alt="Chakra Diagram" style={{ maxWidth: '100%' }} />
-  </div>
-);
-
-export default ChakraDiagram;
